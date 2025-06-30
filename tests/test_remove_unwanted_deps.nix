995ef0159b9d90b4ca56ac5606ae7f44db196bb6
numtel <ben@latenightsketches.com>
2025-06-30 06:34:57 -0700

    Update python 3.9 -> 3.12

--- tests/test_remove_unwanted_deps.nix
+++ tests/test_remove_unwanted_deps.nix
@@ -9,7 +9,7 @@ with builtins;
 # if removing dependency doesn't work this build would raise:
 # Could not find a version that satisfies the requirement packaging (from sphinx)
 mach-nix.mkPython (baseArgsMkPython // {
-  python = "python39";
+  python = "python312";
   requirements = ''
     librosa==0.9.0
   '';
